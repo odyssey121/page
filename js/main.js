ef45afd96cfab3f96af30dbc1b2bde7312930c4a
unknown <odyssey121@mail.ru>
2019-02-24 23:20:32 +0300

    toggle menu on js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log('test');
+// Select dom items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuAvatar = document.querySelector('.menu-avatar');
+const menuUl = document.querySelector('.menu-nav');
+const ulItem = document.querySelectorAll('.nav-item');
+// inital state of menu
+let showMenu = false;
+console.log(ulItem);
+menuBtn.addEventListener('click',Switcher);
+
+function Switcher(){
+    if(!showMenu){
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuAvatar.classList.add('show');
+        menuUl.classList.add('show');
+        ulItem.forEach(item =>
+            item.classList.add('show'));
+
+        showMenu = true;
+    }
+    else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuAvatar.classList.remove('show');
+        menuUl.classList.remove('show');
+        ulItem.forEach(item =>
+            item.classList.remove('show'));
+        showMenu = false;
+    }
+}
+
